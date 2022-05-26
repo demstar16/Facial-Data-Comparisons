f84589577c4838b0ed285b25dd6136930a859d02
demstar16 <44187160+demstar16@users.noreply.github.com>
2022-05-26 22:40:35 +0800

    Added functionality to sort random order of csv data

--- 22988643.py
+++ 22988643.py
@@ -1,40 +1,41 @@
 __author__ = "Dempsey Thompson - 22988643"
 __version__ = "1.0"
 
-def main(csvfile, adultIDs):
-    #read the csvfile
-    data = read_csv(csvfile)
 
-    #make the adultIDs case insensitive
+def main(csvfile, adultIDs):
+    # Read the csvfile
+    data = read_csvfile(csvfile)
+
+    # Make the adultIDs case insensitive
     for i in range(len(adultIDs)):
         adultIDs[i] = adultIDs[i].upper()
 
-    #trim the data, so we just have what we want
+    # Trim the data, so we just have what we want
     wanted_data = data_sort(data, adultIDs)
+    
+    # Splits the adults data up respectively
+    adult1_data = wanted_data[:15]
+    adult2_data = wanted_data[15:]
 
-    #splits the adults up
-    adult1 = wanted_data[:15]
-    adult2 = wanted_data[15:]
-
-    #gain euclidean distances for each adult
-    euclidean_dict1 = euclidean_dist_dict(adult1)
-    euclidean_dict2 = euclidean_dist_dict(adult2)
-
-    #create our OP1 return variable
+    # Gain euclidean distance dictionary for each adult
+    euclidean_dict1 = euclidean_dist_dict(adult1_data)
+    euclidean_dict2 = euclidean_dist_dict(adult2_data)
+   
+    # Create our OP1 return variable
     OP1 = [euclidean_dict1, euclidean_dict2]
-
-    #create our OP2 return variable
+    
+    # Create our OP2 return variable
     OP2 = round(cosine_sim(euclidean_dict1, euclidean_dict2), 4)
-
-    #OP3 functionality
+    
+    # Create our OP3 return variable
     OP3 = []
-    id_data = data_id_sort(data)
+    id_data = id_data_sort(data)
     cosine_comp_list1 = cosine_comparisons(id_data, adultIDs, euclidean_dict1)
     cosine_comp_list2 = cosine_comparisons(id_data, adultIDs, euclidean_dict2)
     OP3.append(cosine_comp_list1)
     OP3.append(cosine_comp_list2)
 
-    #OP4 functionality
+    # Create our OP4 return variable
     OP4 = []
     euclidean_avgs1 = euclidean_avg_dist(cosine_comp_list1, id_data)
     euclidean_avgs2 = euclidean_avg_dist(cosine_comp_list2, id_data)
@@ -43,25 +44,42 @@ def main(csvfile, adultIDs):
 
     return OP1, OP2, OP3, OP4
 
-#function to read all of the csvfile
-def read_csv(csvfile):
+# Function to read all of the csvfile
+def read_csvfile(csvfile):
     data = []
+    order = []
+    for i in range(5):
+        order.append(0)
     try:
         with open(csvfile, 'r') as f:
-            #stores the order of the data given
-            header = f.readline()
+            # Stores the order of the data given
+            header = f.readline().strip('\n').split(',')
+
+            # Orders CSV data into specific format/order --> ['AdultID', 'Landmark', 'X', 'Y', 'Z']
+            for i, heading in enumerate(header):
+                if heading == 'AdultID':
+                    order[0] = i 
+                elif heading == 'Landmark':
+                    order[1] = i 
+                elif heading == 'X':
+                    order[2] = i 
+                elif heading == 'Y':
+                    order[3] = i 
+                elif heading == 'Z':
+                    order[4] = i 
             
-            #obtain all the data
+            # Obtain all the data
             for line in f:
-                currentline = line.split(",")
-                data.append((str(currentline[0]).upper(), str(currentline[1]).upper(), float(currentline[2]), 
-                float(currentline[3]), float(currentline[4])))
+                currentline = line.strip('\n').split(",")
+                data.append((str(currentline[order[0]]).upper(), str(currentline[order[1]]).upper(), 
+                float(currentline[order[2]]), float(currentline[order[3]]), float(currentline[order[4]])))
     except FileNotFoundError:
         print("Error finding or opening the file.")
-  
+        exit(1)
+    
     return data
 
-#function to sort the data from the csvfile
+# Function to return a list only containing data relevant to given adultIDs
 def data_sort(data, adultIDs):
     wanted_data = []
     for i in range(len(data)):
@@ -69,7 +87,7 @@ def data_sort(data, adultIDs):
             wanted_data.append(data[i])
     return wanted_data
 
-#function to help sort the data into what facial area they are for
+# Function to help sort the data into their respective facial feature
 def euclidean_sort(data):
     face_dist_names = ['FW', 'OCW', 'LEFL', 'REFL', 'ICW', 'NW', 'ABW', 'MW', 'NBL', 'NH']
     euclidean_dist = {i: [] for i in face_dist_names}   
@@ -126,9 +144,7 @@ def euclidean_sort(data):
 
     return euclidean_dist
 
-
-
-#3D distance between two points A = (x1, y1, z1) and B = (x2, y2, z2) in cartesian plain stored in a dictionary
+# 3D distance between two points A = (x1, y1, z1) and B = (x2, y2, z2) in cartesian plain stored in a dictionary
 def euclidean_dist_dict(adultID_data):
     face_dist_names = ['FW', 'OCW', 'LEFL', 'REFL', 'ICW', 'NW', 'ABW', 'MW', 'NBL', 'NH']
     euclidean_distances = {i:None for i in face_dist_names}
@@ -152,10 +168,10 @@ def euclidean_dist_dict(adultID_data):
         
         euc_formula = ((val1**2) + (val2**2) + (val3**2))**0.5
         euclidean_distances[abvn] = round(euc_formula, 4)
- 
+  
     return euclidean_distances
 
-#find cosine similarity between faces using the cosine similarity formula
+# Find cosine similarity between faces using the cosine similarity formula
 def cosine_sim(dict1, dict2):
     cosine_similarity = 0
     sum_of_multiples = 0
@@ -169,15 +185,15 @@ def cosine_sim(dict1, dict2):
     cosine_similarity = (sum_of_multiples) / (dict1_sqr_rt_vals * dict2_sqr_rt_vals)
     return cosine_similarity
 
-#basic function to sum items in a list and square root result
+# Function to sum items in a list and square root the result
 def sum_under_sqr_rt(dict):
     sum = 0
     for i in dict.values():
         sum += i**2
     return sum**0.5    
 
-#sort the mass data into a list of lists where each nested list holds all data for a particular ID
-def data_id_sort(data):
+# Sort the mass data into a list of lists where each nested list holds all data for a particular ID
+def id_data_sort(data):
     IDs = []
     sorted_data = [] 
     
@@ -193,7 +209,7 @@ def data_id_sort(data):
                 
     return sorted_data
 
-#function to compare cosine similarities of one face with every other face in data file
+# Function to compare cosine similarities of one face with every other face in data file
 def cosine_comparisons(data, IDs, euclidean_dict):
     list = []
 
@@ -206,13 +222,14 @@ def cosine_comparisons(data, IDs, euclidean_dict):
     list.sort(key=lambda u:(-u[1],u[0]))
     top_5_comparisons = list[:5]
     rounded_t5 = []
-    #for rounding purposes
+    
+    # For rounding purposes
     for i in top_5_comparisons:
         rounded_t5.append((i[0], round(i[1], 4)))
         
     return rounded_t5     
 
-#function for finding the average euclidean distances between faces for each facial feature
+# Function for finding the average euclidean distances between faces for each facial feature
 def euclidean_avg_dist(cosine_comparison_list, data):
     face_dist_names = ['FW', 'OCW', 'LEFL', 'REFL', 'ICW', 'NW', 'ABW', 'MW', 'NBL', 'NH']
     list = []
@@ -226,10 +243,11 @@ def euclidean_avg_dist(cosine_comparison_list, data):
             if data[j][0][0] == list[i]:
                 euclideans.append(euclidean_dist_dict(data[j]))
     for key in avg_dict:
-        avg_dict[key] = round(sum(d[key] for d in euclideans) / len(euclideans), 4)
+      avg_dict[key] = round(sum(d[key] for d in euclideans) / len(euclideans), 4)
+    
     return avg_dict
 
-OP1, OP2, OP3, OP4 = main("sample_face_data.csv", ['r7033', 'P1283'])
+OP1, OP2, OP3, OP4 = main("order_testing.csv", ['r7033', 'P1283'])
 print(f"{OP1=} \n\n {OP2=} \n\n {OP3=} \n\n {OP4=}")
 
 
